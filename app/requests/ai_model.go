88194ff510a7da15cbb98ba95975dbe943c4947d
ChatGPT-X <chatgpt-x@users.noreply.github.com>
2023-12-05 02:54:11 +0800

    优化 代码洁癖

--- app/requests/ai_model.go
+++ app/requests/ai_model.go
@@ -1,13 +1,13 @@
 package requests
 
-// ValidateAiModelCreate 验证 AI 模型表创建验证。
+// ValidateAiModelCreate AI 模型表创建验证。
 type ValidateAiModelCreate struct {
 	DisplayName string `form:"display_name" binding:"required,min=2,max=30"`
 	AiName      string `form:"ai_name" binding:"required,min=2,max=30"`
 	Status      uint   `form:"status" binding:"oneof=0 1"`
 }
 
-// ValidateAiModelUpdate 验证 AI 模型表更新验证。
+// ValidateAiModelUpdate AI 模型表更新验证。
 type ValidateAiModelUpdate struct {
 	ID          uint   `form:"id" binding:"required,numeric"`
 	DisplayName string `form:"display_name" binding:"required,min=2,max=30"`
@@ -15,13 +15,13 @@ type ValidateAiModelUpdate struct {
 	Status      uint   `form:"status" binding:"oneof=0 1"`
 }
 
-// ValidateAiModelSelect 验证 AI 模型表查询验证。
+// ValidateAiModelSelect AI 模型表查询验证。
 type ValidateAiModelSelect struct {
 	Page     int64 `form:"page" binding:"numeric"`
 	PageSize int64 `form:"page_size" binding:"numeric"`
 }
 
-// ValidateAiModelDelete 验证 AI 模型表删除验证。
+// ValidateAiModelDelete AI 模型表删除验证。
 type ValidateAiModelDelete struct {
 	ID uint `form:"id" binding:"required,numeric"`
 }
--- app/requests/user.go
+++ app/requests/user.go
@@ -1,13 +1,13 @@
 package requests
 
-// ValidateDoRegister 验证用户注册。
+// ValidateDoRegister 用户注册验证。
 type ValidateDoRegister struct {
 	Username string `form:"username" binding:"required,alphanum,min=4,max=30"`
 	Email    string `form:"email" binding:"required,email"`
 	Password string `form:"password" binding:"required,min=6,max=30"`
 }
 
-// ValidateDoLogin 验证用户登录。
+// ValidateDoLogin 用户登录验证。
 type ValidateDoLogin struct {
 	Username string `form:"username" binding:"required,alphanum,min=4,max=30"`
 	Password string `form:"password" binding:"required,min=6,max=30"`
